c2a33c43b8ef0787c9ad0a2fdc8b12693e49ad52
RKUCZER <rkuczer@student.bridgew.edu>
2022-10-05 15:49:22 -0400

    new changes made to main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import keyboard
-import time
+
 
 keys = [] #initializes the list of 10 keys to send to log.txt when limit is reached
 
-hotkey1 = keyboard.add_hotkey('ctrl + shift + z', lambda: print("shift+a was Pressed!")) #creates hotkey1 when shift+ctrl+z is pressed
+hotkey1 = keyboard.add_hotkey('ctrl + shift + z', lambda: print("ctrl + shift + z was Pressed!")) #creates hotkey1 when shift+ctrl+z is pressed
 hotkey2 = keyboard.add_hotkey('shift + a', lambda: print("shift+a was Pressed!")) #creates hotkey2 when shift+s is pressed
 
 def key_pressed(event): #creates event handler function when key is pressed
@@ -21,4 +21,4 @@ def key_pressed(event): #creates event handler function when key is pressed
 
 keyboard.on_press(key_pressed) #event handler for when key pressed to call the function made above12234567890
 keyboard.wait() #waits for next key pressed
-time.sleep(30) #has program terminate after 30 seconds
+
